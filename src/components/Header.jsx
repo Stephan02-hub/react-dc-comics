fb3ebd0977497765010353e8519512eb923e04a4
Stephan02-hub <braghesesteven@gmail.com>
2025-01-29 17:30:53 +0100

    add: feat header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,46 @@
 export default function Header() {
-    return (
+    return (<header>
     <div className="container">
-        <img className="logo-img" src="./image/dc-logo.png" alt="logo"></img>
+        <img className="logo-img" src="/dc-logo.png" alt="logo"></img>
+        <nav className="header-nav">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li>
+                    <a  href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
     </div>
+    <div className="black">
+        <h1 >--&gt; Content goes here &lt;--</h1>
+        </div>
+</header>
+    
 )
 }
