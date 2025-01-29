9e001215f026307ca936100c8a4c2a1c89d806a9
Stephan02-hub <braghesesteven@gmail.com>
2025-01-29 18:43:54 +0100

    add:feat main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,28 @@
 export default function Main() {
-    return <main></main>
+    return <main>
+        <section className="main">
+            <div className="upper">
+            <div className="col">
+            <img src="/public/buy-comics-digital-comics.png" alt="" />
+            <h3>Digital Comics</h3>
+            </div>
+             <div className="col">
+            <img src="/public/buy-comics-merchandise.png" alt="" />
+            <h3>Dc Merchendise</h3>
+             </div>
+             <div className="col">
+            <img src="/public/buy-comics-subscriptions.png" alt="" />
+            <h3>Subscriptions</h3>
+            </div>
+            <div className="col">
+            <img src="/public/buy-comics-shop-locator.png" alt="" />
+            <h3>Comics Shop Locar</h3>
+             </div>
+             <div className="col">
+            <img src="/public/buy-dc-power-visa.svg" alt="" />
+            <h3>Dc Power Visa</h3>
+        </div>
+        </div>
+        </section>
+    </main>
 }
