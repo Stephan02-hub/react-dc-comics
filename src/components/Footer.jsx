61583d413c93d252f5d339d7f8462ed716166993
Stephan02-hub <braghesesteven@gmail.com>
2025-01-29 22:44:46 +0100

    add:feat footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,122 @@
 export default function Footer() {
-    return <footer></footer>
+    return <footer>
+          <div className="up footer">
+        <div className="container-footer">
+          <div className="first-col">
+            <h3>DC COMICS</h3>
+            <ul>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+            <h3 className="second-title">SHOP</h3>
+            <ul>
+              <li>
+                <a href="#">Shop DC</a>
+              </li>
+              <li>
+                <a href="#">Shop DC Collectibles</a>
+              </li>
+            </ul>
+          </div>
+          <div className="second-col">
+            <h3>DC</h3>
+            <ul>
+              <li>
+                <a href="#">Terms of use</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy (New)</a>
+              </li>
+              <li>
+                <a href="#">Ad choices</a>
+              </li>
+              <li>
+                <a href="#">Advertising</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Subscriptions</a>
+              </li>
+              <li>
+                <a href="#">Talent workshop</a>
+              </li>
+              <li>
+                <a href="#">CPSC Certificates</a>
+              </li>
+              <li>
+                <a href="#">Ratings</a>
+              </li>
+              <li>
+                <a href="#">Shop Help</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="third-col">
+            <h3 className="third-title">SITES</h3>
+            <ul>
+              <li>
+                <a href="#">DC</a>
+              </li>
+              <li>
+                <a href="#">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="#">DC Kids</a>
+              </li>
+              <li>
+                <a href="#">DC Universe</a>
+              </li>
+              <li>
+                <a href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+          <div className="img-footer">
+            <img src="/public/dc-logo-bg.png" alt="logo" />
+          </div>
+        </div>
+      </div>
+      <div className="down-footer">
+        <div className="container-down-footer">
+          <div className="left">
+            <button className="sign-up">SIGN-UP NOW!</button>
+          </div>
+          <div className="right">
+            <h3>FOLLOW-US</h3>
+            <div className="img-down-footer">
+              <img src="/public/footer-facebook.png" alt="" />
+              <img src="/public/footer-twitter.png" alt="" />
+              <img src="/public/footer-youtube.png" alt="" />
+              <img src="/public/footer-pinterest.png" alt="" />
+              <img src="/public/footer-periscope.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </footer>
 }
