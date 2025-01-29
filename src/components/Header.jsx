49ccac0e531b05d0da3dfcf78dca1a567f228777
Stephan02-hub <braghesesteven@gmail.com>
2025-01-29 16:00:19 +0100

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
     <div className="container">
-        <img className="logo-img" src=".image/dc-logi.png" alt="logo"></img>
+        <img className="logo-img" src="./image/dc-logo.png" alt="logo"></img>
     </div>
 )
 }
